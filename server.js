8a608d817f4e9a78f5d2c5ee56501c662d9feab6
Sera De La Rosa <sera.delarosa@outlook.com>
2025-02-28 11:20:33 -0500

    Debug MONGODB_URI for Heroku

--- server.js
+++ server.js
@@ -21,11 +21,19 @@ const port = process.env.PORT ? process.env.PORT : '3000';
 
 // =================================================================================================
 
-mongoose.connect(process.env.MONGODB_URI);
+//debugging for heroku
+console.log("MONGODB_URI:", process.env.MONGODB_URI); 
 
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log(`Connected to MongoDB ${mongoose.connection.name}.`))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+
+// mongoose.connect(process.env.MONGODB_URI);
+
+// mongoose.connection.on('connected', () => {
+//   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+// });
 
 // ==================================================================================================
 
